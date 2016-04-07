03118ce2c300e89badbe5aa7f2c066d173845f41
Noah Kittleson <noahkittleson@gmail.com>
2016-04-07 16:52:01 -0700

    added variable length, height, and connection-to-win amount

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,32 @@
 
 $(document).ready(function() {
 
-  board = [[], [], [], [], [], [], []];
-  var height = 5;
-  var ex = true;
+  // board = [[], [], [], [], [], [], []];
+  // var height = 5;
+  // var ex = true;
 
-  function drawBoard() {
+function Board(cols, rows, winNumber) {
+  this.height = parseInt(rows) - 1;    //?????
+  this.board = [];
+  for (var i = 0; i < parseInt(cols); i++) {
+    this.board.push([]);
+  }
+  this.winStringX = "";
+  this.winStringO = "";
+  for (var i = 0; i < parseInt(winNumber); i++) {
+    this.winStringX = this.winString.concat("x");
+    this.winStringO = this.winString.concat("o");
+  }
+  this.ex = true;
+}
+
+  Board.prototype.drawBoard = function() {
     $("#board").empty();
     var appendedText = "<p>";
-    for (var row = 0; row <= height; row++) {
-      for (var col = 0; col < board.length; col++) {
-        if (board[col][height-row] != undefined) {
-          appendedText = appendedText.concat("| " + board[col][height-row] + " ");
+    for (var row = 0; row <= this.height; row++) {
+      for (var col = 0; col < this.board.length; col++) {
+        if (this.board[col][this.height-row] != undefined) {
+          appendedText = appendedText.concat("| " + this.board[col][this.height-row] + " ");
         } else {
           appendedText = appendedText.concat("| - ");
         }
@@ -22,25 +37,25 @@ $(document).ready(function() {
     $("#board").append(appendedText);
   }
 
-  function placePiece(position) {
-    if (board[position].length <= height) {
-      if (ex) {
-        board[position].push("x");
+  Board.prototype.placePiece = function(position) {
+    if (this.board[position].length <= this.height) {
+      if (this.ex) {
+        this.board[position].push("x");
       } else {
-        board[position].push("o");
+        this.board[position].push("o");
       }
     } else {
       alert("Invalid Move");
     }
   }
 
-  function checkForWin(x, y) {
+  Board.prototype.checkForWin = function(x, y) {
     console.log("NEW TURN");
     //check horizontal
     var horizontal = "";
     for (var row = x-3; row <= x+3; row++) {
-      if (board[row] != undefined && board[row][y] != undefined) {
-        horizontal = horizontal.concat(board[row][y]);
+      if (this.board[row] != undefined && this.board[row][y] != undefined) {
+        horizontal = horizontal.concat(this.board[row][y]);
       } else {
         horizontal = horizontal.concat("-");
       }
@@ -49,8 +64,8 @@ $(document).ready(function() {
     //check vertical
     var vertical = "";
     for (var col = y-3; col <= y+3; col++) {
-      if (board[x] != undefined && board[x][col] != undefined) {
-        vertical = vertical.concat(board[x][col]);
+      if (this.board[x] != undefined && this.board[x][col] != undefined) {
+        vertical = vertical.concat(this.board[x][col]);
       } else {
         vertical = vertical.concat("-");
       }
@@ -61,14 +76,14 @@ $(document).ready(function() {
     var diagonalDown = "";
     for (var num = -3; num <= 3; num++) {
       //diagonal up
-      if (board[x+num] != undefined && board[x+num][y+num] != undefined) {
-        diagonalUp = diagonalUp.concat(board[x+num][y+num]);
+      if (this.board[x+num] != undefined && this.board[x+num][y+num] != undefined) {
+        diagonalUp = diagonalUp.concat(this.board[x+num][y+num]);
       } else {
         diagonalUp = diagonalUp.concat("-");
       }
       // diagonal down
-      if (board[x+num] != undefined && board[x+num][y-num] != undefined) {
-        diagonalDown = diagonalDown.concat(board[x+num][y-num]);
+      if (this.board[x+num] != undefined && this.board[x+num][y-num] != undefined) {
+        diagonalDown = diagonalDown.concat(this.board[x+num][y-num]);
       } else {
         diagonalDown = diagonalDown.concat("-");
       }
@@ -79,40 +94,53 @@ $(document).ready(function() {
     if (vertical.search("xxxx") != -1 || vertical.search("oooo") != -1) {
       alert("Vertical Win");
       return true;
-    } else if (horizontal.search("xxxx") != -1 || horizontal.search("oooo") != -1) {
+    } else if (horizontal.search(this.winStringX) != -1 || horizontal.search(this.winStringO) != -1) {
       alert("Horizontal Win");
       return true;
-    } else if (diagonalUp.search("xxxx") != -1 || diagonalUp.search("oooo") != -1) {
+    } else if (diagonalUp.search(this.winStringX) != -1 || diagonalUp.search(this.winStringO) != -1) {
       alert("Diagonal-Up Win");
       return true;
-    } else if (diagonalDown.search("xxxx") != -1 || diagonalDown.search("oooo") != -1) {
+    } else if (diagonalDown.search(this.winStringX) != -1 || diagonalDown.search(this.winStringO) != -1) {
       alert("Diagonal-Down Win");
       return true;
     }
     return false;
   }
 
-  function getPlayer() {
-    if (ex) {
+  Board.prototype.isTie = function() {
+    for (var i = 0; i < this.board.length; i++) {
+      if (this.board[i].length <= this.height) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  Board.prototype.getPlayer = function() {
+    if (this.ex) {
       return "X"
     } else {
       return "O"
     }
   }
 
-  drawBoard();
-  $("#message").text(getPlayer() + "'s Turn");
+  var gameBoard = new Board(7, 6, 4);
+  console.log(gameBoard.board);
+  gameBoard.drawBoard();
+  $("#message").text(gameBoard.getPlayer() + "'s Turn");
 
   $("form").submit(function(event){
     event.preventDefault();
     var place = parseInt($("input:radio[name=col]:checked").val());
-    placePiece(place);
-    drawBoard();
-    if (checkForWin(place, board[place].length-1)) {
-      $("#message").text(getPlayer() + " Wins!");
+    gameBoard.placePiece(place);
+    gameBoard.drawBoard();
+    if (gameBoard.checkForWin(place, gameBoard.board[place].length-1)) {
+      $("#message").text(gameBoard.getPlayer() + " Wins!");
+    } else if (gameBoard.isTie()) {
+      $("#message").text("Tie Game!");
     } else {
-      $("#message").text(getPlayer() + "'s Turn");
+      $("#message").text(gameBoard.getPlayer() + "'s Turn");
     }
-    ex = !ex;
+    gameBoard.ex = !gameBoard.ex;
   });
 });
